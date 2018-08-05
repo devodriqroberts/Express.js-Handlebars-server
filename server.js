18739965f2d983145ae6576a918a6cce3361f047
DevoRoberts <devorob26@gmail.com>
2018-08-05 01:22:39 -0400

    Initial commit express.js

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+//middleware
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server log.')
+        }
+    });
+
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs', {
+        pageTitle: 'Sorry this page is currently down for maintenance. Will be back up soon!'
+    })
+})
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+
+app.get('/', (req, res) => {
+    
+    res.render('home.hbs', {
+        pageTitle: 'This is my home page',
+        welcomeMessage: 'Welcome!',
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+
+    })
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        bad: "Bad request route"
+    })
+})
+
+
+
+
+app.listen(3000);
